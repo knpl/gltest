13f51ab845574720c0eb07b14de5b9a2c24c6eb9
Rene van Klink <knplnl@gmail.com>
2019-02-13 23:43:57 +0100

    Added vertex and fragment shaders

--- glfw_test/main.cpp
+++ glfw_test/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include <exception>
-#include <Windows.h>
 
+#include <vector>
 // GLFW header should be last (It detects the presence of other headers
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -10,6 +12,8 @@
 #define WINDOW_HEIGHT 720
 
 #define WINDOW_TITLE "New window"
+
+#pragma comment(lib, "glew32.lib")
 
 void error_callback(int error, const char *message) {
 	std::cerr << message;
@@ -21,9 +25,126 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 	}
 }
 
+std::string get_shader_code(std::string const &path) {
+	std::ifstream in_stream(path, std::ios::in);
+	if (!in_stream.is_open()) {
+		std::stringstream strstr;
+		strstr << "Failed to open file with path: " << path;
+		throw std::runtime_error(strstr.str());
+	}
+
+	std::stringstream strstr;
+	strstr << in_stream.rdbuf();
+	std::string code = strstr.str();
+	in_stream.close();
+
+	return code;
+}
+
+void compile_shader(GLuint shader_id, std::string const& shader_code) {
+
+	char const* shader_code_ptr = shader_code.c_str();
+	glShaderSource(shader_id, 1, &shader_code_ptr, NULL);
+	glCompileShader(shader_id);
+
+	GLint result;
+	int info_log_length;
+	glGetShaderiv(shader_id, GL_COMPILE_STATUS, &result);
+
+	if (result == GL_FALSE) {
+		std::stringstream strstr;
+		glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &info_log_length);
+
+		std::vector<char> buffer(info_log_length + 1);
+		glGetShaderInfoLog(shader_id, info_log_length, NULL, &buffer[0]);
+		buffer[info_log_length] = '\0';
+
+		strstr << "Compilation failed: " << std::string(buffer.begin(), buffer.end());
+		throw std::runtime_error(strstr.str());
+	}
+}
+
+void link_shader_program(GLuint program_id) {
+	glLinkProgram(program_id);
+
+	GLint result;
+	int info_log_length;
+	glGetProgramiv(program_id, GL_LINK_STATUS, &result);
+	
+	if (result == GL_FALSE) {
+		std::stringstream strstr;
+		glGetProgramiv(program_id, GL_INFO_LOG_LENGTH, &info_log_length);
+
+		std::vector<char> buffer(info_log_length + 1);
+		glGetProgramInfoLog(program_id, info_log_length, NULL, &buffer[0]);
+		buffer[info_log_length] = '\0';
+
+		strstr << "Linking failed: " << std::string(buffer.begin(), buffer.end());
+		throw std::runtime_error(strstr.str());
+	}
+}
+
+GLuint loadShaderProgram(std::string const& vertex_shader_path, std::string const& fragment_shader_path) {
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+
+	std::string vs_code = get_shader_code(vertex_shader_path);
+	std::string fs_code = get_shader_code(fragment_shader_path);
+
+	compile_shader(vs, vs_code);
+	compile_shader(fs, fs_code);
+
+	GLuint prog = glCreateProgram();
+	glAttachShader(prog, vs);
+	glAttachShader(prog, fs);
+
+	glLinkProgram(prog);
+
+	// clean up shader objects.
+	glDetachShader(prog, vs);
+	glDetachShader(prog, fs);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return prog;
+}
+
 void start(GLFWwindow *window) {
 	glfwSwapInterval(1);
+
+	glClearColor(0., 0., 0.4, 0.);
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	float buffer_data[] = {
+		-1., -1., 0.,
+		1., -1., 0.,
+		0., 1., 0.
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), buffer_data, GL_STATIC_DRAW);
+
+	GLuint prog = loadShaderProgram(
+		"C:\\Users\\rene\\source\\repos\\glfw_test\\glfw_test\\vertex.vs", 
+		"C:\\Users\\rene\\source\\repos\\glfw_test\\glfw_test\\fragment.fs");
+
 	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glUseProgram(prog);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -38,9 +159,9 @@ void initGlfw() {
 
 		glfwSetErrorCallback(error_callback);
 
-		
-		// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-		// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		glfwWindowHint(GLFW_SAMPLES, 4); // 4x anti-aliasing
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		
 
 		GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
@@ -50,6 +171,8 @@ void initGlfw() {
 		try {
 			glfwMakeContextCurrent(window);
 			glfwSetKeyCallback(window, key_callback);
+
+			glewExperimental = true;
 
 			if (glewInit() != GLEW_OK) {
 				throw std::runtime_error("Failed to initialize glew");
@@ -67,7 +190,7 @@ void initGlfw() {
 		glfwTerminate();
 	}
 	catch (std::runtime_error &ex) {
-		std::cerr << "Message: " << ex.what();
+		std::cerr << "Message: " << ex.what() << std::endl;
 		glfwTerminate();
 	}
 }
@@ -75,6 +198,6 @@ void initGlfw() {
 int main(int argc, char **argv) {
 	initGlfw();
 	char c;
-	std::cout << "Enter a key...";
+	std::cout << "Enter a key..." << std::endl;
 	std::cin >> c;
 }
---
+++ glfw_test/vertex.vs
@@ -0,0 +1,7 @@
+#version 330 core
+layout(location = 0) in vec3 vertex_pos;
+
+void main() {
+	gl_Position.xyz = vertex_pos;
+	gl_Position.w = 1.0;
+}
